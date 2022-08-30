bc36be6fe8e0827e0110ce9c20eda37bb75a68a2
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-08-30 09:21:49 -0300

    DEPLOY - fix ci pr.yml

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}. yaiiiiii`);
+  console.log(`Server is listening on port ${PORT}. 2 yaiiiiii`);
 });
